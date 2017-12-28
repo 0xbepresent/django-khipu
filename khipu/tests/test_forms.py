16b2b9f4b98feb0aac3abc4e5f7f30f5f021af53
Misa G <hi@misalabs.com>
2017-12-28 12:53:25 -0600

    Refactoring :squirrel:

--- khipu/tests/test_forms.py
+++ khipu/tests/test_forms.py
@@ -2,6 +2,7 @@ import mock
 
 from django.test import TestCase
 
+from ..exceptions import KhipuError
 from ..forms import KhipuCreatePaymentForm
 
 
@@ -12,9 +13,14 @@ class TestForms(TestCase):
         Test para el form que ayuda a crear un formulario Khipu para obener
         una orden de pago.
         """
+        # Los campos amount y currency son necesarios.
+        with self.assertRaises(KhipuError):
+            KhipuCreatePaymentForm(**{})
+
         # La conexion no se puede hacer con Khipu.
         with mock.patch('khipu.services.common.KhipuService.request'):
-            f = KhipuCreatePaymentForm(**{})
+            f = KhipuCreatePaymentForm(**{
+                'amount': 1000, 'currency': 'CLP', 'subject': 'my subject'})
             self.assertIsNone(f.payment_url)
             self.assertIsNone(f.transfer_url)
             self.assertIn("Servicio no disponible", f.render())
@@ -26,6 +32,7 @@ class TestForms(TestCase):
                     return_value={
                         'payment_url': 'https://testing.com/a',
                         'transfer_url': 'https://testings.com/b'}):
-            f = KhipuCreatePaymentForm(**{})
+            f = KhipuCreatePaymentForm(
+                **{'amount': 1000, 'currency': 'CLP', 'subject': 'my subject'})
             self.assertIn("id_khipu_form_banco", f.render())
             self.assertIn("id_khipu_form_transferencia", f.render())
