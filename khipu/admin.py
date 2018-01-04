c554e6c5425d43363d43727a234d10a9d05dcc67
Misa G <hi@misalabs.com>
2018-01-03 20:25:57 -0600

    Adding Khipu Admin

---
+++ khipu/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    """
+    Modelo de administracion de Ordenes de Khipu
+    """
+    list_display = (
+        "id", "payment_id", "subject", "currency", "amount",
+        "status"
+    )
+    search_fields = ["payment_id"]
+    list_per_page = 100
+
+admin.site.register(Payment, PaymentAdmin)
